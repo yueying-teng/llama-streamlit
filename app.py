8ab2c6b886dff72a6e735e44480dd40ca72c5e2f
yueying-teng <yt2495@columbia.edu>
2023-07-24 17:25:30 +0900

    switch to llama-2-13b-chat

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# Bring in deps
 import streamlit as st
 from langchain.llms import LlamaCpp
-from langchain.embeddings import LlamaCppEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 from langchain.chains import LLMChain
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
-# Customize the layout
-st.set_page_config(page_title="DOCAI", page_icon="🤖", layout="wide", )
+st.set_page_config(page_title="Hashtag Generator", page_icon="#️⃣#️⃣#️⃣#️⃣", layout="wide", )
 st.markdown(f"""
             <style>
             .stApp {{background-image: url("https://images.unsplash.com/photo-1612538498456-e861df91d4d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
@@ -19,51 +19,38 @@ st.markdown(f"""
          </style>
          """, unsafe_allow_html=True)
 
-# function for writing uploaded file in temp
-def write_text_file(content, file_path):
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-        return True
-    except Exception as e:
-        print(f"Error occurred while writing the file: {e}")
-        return False
-
 # set prompt template
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """You are a human who is trying to sell an item on an e-commerce platform and you need to come up with five hashtags that best describe the item.
 
-{context}
+Come up with five hashtags based on the item's title that will help you reach a larger amount of buyers and get more engagement.
+"""
 
-Question: {question}
-Answer:"""
-prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+system_message_prompt = SystemMessagePromptTemplate.from_template(prompt_template)
 
-# initialize the LLM & Embeddings
-llm = LlamaCpp(model_path="./models/llama-2-13b.ggmlv3.q4_0.bin")
-embeddings = LlamaCppEmbeddings(model_path="models/llama-2-13b.ggmlv3.q4_0.bin")
-llm_chain = LLMChain(llm=llm, prompt=prompt)
+human_template = """List five hashtags for this item: {title}"""
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-st.title("📄 Document Conversation 🤖")
-uploaded_file = st.file_uploader("Upload an article", type="txt")
+chat_prompt = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+)
 
-if uploaded_file is not None:
-    content = uploaded_file.read().decode('utf-8')
-    # st.write(content)
-    file_path = "temp/file.txt"
-    write_text_file(content, file_path)
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+llm = LlamaCpp(
+    # model_path="./models/llama-2-13b.ggmlv3.q4_0.bin",
+    model_path="./models/llama-2-13b-chat.ggmlv3.q4_0.bin",
+    temperature=0.7,
+    repeat_penalty=1.176,
+    max_tokens=2048,
+    top_p=0.1,
+    top_k=40,
+    seed=2023,
+    callback_manager=callback_manager,
+    verbose=True,
+)
+llm_chain = LLMChain(llm=llm, prompt=chat_prompt)
 
-    loader = TextLoader(file_path)
-    docs = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    texts = text_splitter.split_documents(docs)
-    db = Chroma.from_documents(texts, embeddings)
-    st.success("File Loaded Successfully!!")
-
-    # Query through LLM
-    question = st.text_input("Ask something from the file", placeholder="Find something similar to: ....this.... in the text?", disabled=not uploaded_file,)
-    if question:
-        similar_doc = db.similarity_search(question, k=1)
-        context = similar_doc[0].page_content
-        query_llm = LLMChain(llm=llm, prompt=prompt)
-        response = query_llm.run({"context": context, "question": question})
-        st.write(response)
+st.title("#️⃣#️⃣#️⃣#️⃣")
+input_title = st.text_input("enter listing's title", placeholder="Nike Court Royal",)
+if input_title:
+    response = llm_chain.run(title=input_title)
+    st.write(response)
