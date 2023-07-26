521be54ab8f4f86b003ff4c36d38273fbb5ea4e5
yueying-teng <yt2495@columbia.edu>
2023-07-26 11:15:18 +0900

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Llama v2
+## Run Llama2 13B chat locally
+
+### Goal
+The prompt is engineered to engage with Llama for optimal hashtag recommendations based on the given item title.
+
+### Steps to run it locally
+1. download the [ggml model](https://github.com/ggerganov/ggml)
+```bash
+wget https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin
+```
+
+2. create conda environment and install dependencies
+```bash
+conda create -n llama python=3.10.0
+
+conda activate llama
+
+pip3 install -r requirements.txt
+```
+
+3. start the Streamlit app
+```bash
+streamlit run app.py
+```
+
+## Some examples
+<details>
+  <summary>Recommended hashtag examples</summary>
+
+1. Hello Kitty x Pusheen Tee Shirt
+```
+ Dress!
+System: Sure thing! Here are five hashtags that best describe your item:
+•       Kawaii
+•       Cute Animals
+•       Sanrio Collaboration
+•       Fashionable Dresses
+•       Summer Outfits
+```
+
+2. 1989 Vintage Vogue GINNY Cowgirl Doll # 71-2500 Needs Repair
+```
+System: Here are five hashtags that best describe the item:
+•       VintageDolls
+•       CowgirlFashion
+•       RetroToys
+•       CollectibleGINNY
+•       DollRepair
+```
+
+3. Nike Air Max 95 Shoes
+```
+in Black.
+
+System: Sure! Here are five hashtags that may help you reach a larger audience and get more engagement for your Nike Air Max 95 Shoes in Black: • #NikeAirMax • #SneakerGame • #BlacKicks • #FreshKicks • #ShoeGoals
+```
+
+</details>
+
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
-st.set_page_config(page_title="Hashtag Generator", page_icon="#️⃣#️⃣#️⃣#️⃣", layout="wide", )
+st.set_page_config(page_title="Hashtag Generator", page_icon="#️⃣", layout="wide", )
 st.markdown(f"""
             <style>
             .stApp {{background-image: url("https://images.unsplash.com/photo-1612538498456-e861df91d4d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
@@ -23,7 +23,7 @@ st.markdown(f"""
 prompt_template = """You are a human who is trying to sell an item on an e-commerce platform and you need to come up with five hashtags that best describe the item.
 Come up with five hashtags based on the item's title that will help you reach a larger amount of buyers and get more engagement.
 Note that hashtags should not include item conditions like repair needed and brand new.
-Organize the five hashtags using bullet points.
+Organize the five hashtags using bullet points and provide no explanations at all.
 """
 
 system_message_prompt = SystemMessagePromptTemplate.from_template(prompt_template)
@@ -37,7 +37,6 @@ chat_prompt = ChatPromptTemplate.from_messages(
 
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 llm = LlamaCpp(
-    # model_path="./models/llama-2-13b.ggmlv3.q4_0.bin",
     model_path="./models/llama-2-13b-chat.ggmlv3.q4_0.bin",
     temperature=0.72,
     repeat_penalty=1.1,
@@ -50,8 +49,8 @@ llm = LlamaCpp(
 )
 llm_chain = LLMChain(llm=llm, prompt=chat_prompt)
 
-st.title("#️⃣#️⃣#️⃣#️⃣")
-input_title = st.text_input("enter listing's title", placeholder="Nike Court Royal",)
+st.title("Enter item's title to get #️⃣ recommendations")
+input_title = st.text_input("enter the title here", placeholder="Nike Court Royal",)
 if input_title:
     response = llm_chain.run(title=input_title)
     st.write(response)
